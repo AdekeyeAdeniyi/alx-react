081cd7f6d413367be85fd6102faac6627780e5f8
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-01-25 19:24:24 +0100

    [fix :rocket:] - code issues

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'production',
+
+    entry:{
+        main: path.resolve(`${__dirname}/js/dashboard_main.js`)
+    },
+
+    output:{
+        path: path.resolve(`${__dirname}/public`),
+        filename: 'bundle.js'
+    },
+
+    performance: {
+		maxAssetSize: 1000000,
+	},
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(gif|svg|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
