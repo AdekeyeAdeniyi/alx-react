5e4a6cbafc0a17ee11d5ed819784a7222d819870
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-01-28 00:11:30 +0100

    [feat :dart:] - add email and password inputs

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,30 @@
+import logo from './holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" width={200} height={200} />
+        <h1> School dashboard </h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email"> Email </label>
+          <input type="text" id="email" />
+          <label htmlFor="password"> Password </label>
+          <input type="text" id="password" />
+
+          <input type="submit" value="Ok" />
+        </form>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
